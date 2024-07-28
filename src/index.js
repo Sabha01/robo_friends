446630f6ba9d17e317ba57b721feb0cb8805f77a
Sabha <sabhamogaveera0@gmail.com>
2024-07-28 17:38:12 +0530

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'tachyons';
-import App from "./Containers/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
